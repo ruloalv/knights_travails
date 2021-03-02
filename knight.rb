58c17057f9bf24bdcc4075f8be541139fba9ffc3
ruloalv <ruloalv@gmail.com>
2021-03-02 10:01:50 -0300

    create initialize for knight class

--- knight.rb
+++ knight.rb
@@ -1,10 +1,13 @@
 class knight
-	MOVES = [[-1,+2],[-1,-2],[+1,+2],[+1,-2],[+2,-1],[+2,+1],[-2,-1],[-2,+1]]
+	
 	attr_reader :position, :from, :history
+	MOVES = [[-1,+2],[-1,-2],[+1,+2],[+1,-2],[+2,-1],[+2,+1],[-2,-1],[-2,+1]]
 
 	def initialize(position = [0,0])
 		@position = position
 		@from = position
 		@history = [].push(position)
 	end
+
+	
 end
